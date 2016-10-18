b08831e1bc12c5392c694f3ddb485e9da5ba2eb5
ianafeemuguiro <ianafeyyy@gmail.com>
2016-10-18 16:40:05 -0500

    Intento de utilizar span en el html

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,3 +1,8 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+var nombre, apellido, correo, contraseña;
+nombre = document.getElementById ('nombre').value; 
+apellido = document.getElementById ('apellido').value;
+correo = document.getElementById ('correo').value;
+contraseña = document.getElementById ('contraseña').value;
+
 }
