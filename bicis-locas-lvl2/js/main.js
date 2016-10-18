817a5e911b05ea6c9bcf27814c27a2562b1835d9
ianafeemuguiro <ianafeyyy@gmail.com>
2016-10-18 16:59:55 -0500

    Adelanto

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -5,4 +5,9 @@ apellido = document.getElementById ('apellido').value;
 correo = document.getElementById ('correo').value;
 contraseña = document.getElementById ('contraseña').value;
 
+if (nombre == null || nombre.length == 0)
+var errorNombre = document.createElement ("span"); 
+var errorText = document.createTextNode ("Campo vacio");
+errorNombre.appendChild ('textError');
+document.getElementByTagName ('div')[1].appendChild (errorNombre);
 }
